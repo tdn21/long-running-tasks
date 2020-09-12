414b609c5615bfb3849f1c463326b6575a632a9f
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-09-13 00:06:49 +0530

    updated index.js with required routers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
+const uploadRouter = require('./src/routers/upload');
+const teamRouter = require('./src/routers/team');
+const extractRouter = require('./src/routers/extract');
+
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+
+app.use(uploadRouter);
+app.use(teamRouter);
+app.use(extractRouter);
 
 app.listen(port, () => {console.log(`Server is up on port ${port}`)});
