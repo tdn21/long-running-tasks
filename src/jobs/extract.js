86da20984ad5e9cb25a38e6bfaf64181add8e8e7
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-09-12 23:23:38 +0530

    added extract module for child process

---
+++ src/jobs/extract.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const readline = require('readline');
+
+// function to increase execution time to simulate long running process
+const long_running_function = (start) => {
+    if(!start)
+        start = 0;
+    let counter = start;
+    while(counter < 100000000) {
+        counter++;
+    }
+}
+
+// demonstration extraction process
+const extractData = (start, end) => {
+    var count = 1;
+
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream('./data/demo_data.csv'),
+        console: false
+    });
+
+    readInterface.on('line', function(line) {
+        if(count > start && count <= end) {
+            var message = {
+                "status" : "Ongoing",
+                "state" : count,
+                "data" : {[count] : line}
+            }
+            long_running_function(0);
+            process.send(message);
+        }
+        count++;
+    });
+
+    readInterface.on('close', () => {
+        var message = {"status" : "Success"};
+        process.send(message);
+        process.exit(0);
+    });
+}
+
+// To initiate extraction process on 'START'
+process.on('message', (message) => {
+    if(message == 'START') {
+        console.log('Extract Child process received: START message');
+        extractData(process.argv[2], process.argv[3]);
+    }
+});
